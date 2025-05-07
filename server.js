f22594d488435a124c2f2b2c0477959e82309084
Zaheer Ahmed <ranazaheerahmed65@gmail.com>
2025-05-07 17:32:01 +0500

    new deploymnet

--- server.js
+++ server.js
@@ -3,33 +3,37 @@ const app = express();
 const path = require('path');
 const port = 3000;
 const cors = require('cors');
-const { format } = require('date-fns');
-const authRoutes = require("./routes/authRoutes")
-const postsRoutes = require("./routes/postsRoutes")
-const userRoutes = require("./routes/userRoutes")
-const multer = require('multer');
+const authRoutes = require("./routes/authRoutes");
+const postsRoutes = require("./routes/postsRoutes");
+const userRoutes = require("./routes/userRoutes");
 const cookieParser = require('cookie-parser');
-const authenticate = require('./middlware/authenticate');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors());
+const allowedOrigins = [
+    'http://localhost',
+    'http://localhost:3000',
+    'https://node-deployement-apis.vercel.app'
+];
+
 app.use(cors({
-    origin: ['http://localhost', 'http://localhost:3000', 'http://your-cordova-app-url'], // Adjust with your Cordova app URL
-    credentials: true
+    origin: allowedOrigins,
+    credentials: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.use("/auth", authRoutes);
 app.use("/posts", postsRoutes);
-app.use("/users",userRoutes);
+app.use("/users", userRoutes);
+
 app.get("/", (req, res) => {
     res.json({ message: 'Welcome to the API' });
 });
+
 app.get("*", (req, res) => {
     res.status(404).json({ error: 'Page not found' });
 });
